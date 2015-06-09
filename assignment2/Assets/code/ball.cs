8a5e717e040f987a8e48532a030e25ae19405902
Nicholas Farrugia <Nerist@Nicholass-MacBook-Pro.local>
2015-06-09 15:11:41 +0200

    BUG fix
    Fixed the bug were the ball was not respawning.

--- assignment2/Assets/code/ball.cs
+++ assignment2/Assets/code/ball.cs
@@ -28,7 +28,10 @@ public class ball : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
+		if (transform.position.y < -10) {
+			transform.position = new Vector3 (0f, 0f, 0f);
+			transform.rotation = Quaternion.Euler (0f, 0f,130f);
+			GM.lives--;
 		}
 
 
