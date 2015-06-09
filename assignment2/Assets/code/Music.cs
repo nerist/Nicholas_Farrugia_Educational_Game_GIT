5d709e3e8f87a846273f32369a4055bc9f898afb
Nicholas Farrugia <Nerist@Nicholass-MacBook-Pro.local>
2015-06-09 14:27:38 +0200

    Game
    Early version of the game

---
+++ assignment2/Assets/code/Music.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+	public AudioClip tech;
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
